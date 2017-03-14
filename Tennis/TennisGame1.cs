2a97a5f62a129f7508117940ffb21434ca4fbe62
Justin <greyepoxy2@gmail.com>
2017-03-14 13:28:15 -0700

    R#:extract methods

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Tennis
 {
 	class TennisGame1 : ITennisGame
@@ -25,7 +27,8 @@ namespace Tennis
 		{
 			string score = "";
 			var tempScore = 0;
-			if (m_score1 == m_score2) {
+			if (m_score1 == m_score2)
+			{
 				switch (m_score1) {
 					case 0:
 						score = "Love-All";
@@ -39,37 +42,60 @@ namespace Tennis
 					default:
 						score = "Deuce";
 						break;
-
 				}
 			}
-			else if (m_score1 >= 4 || m_score2 >= 4) {
+			else if (m_score1 >= 4 || m_score2 >= 4)
+			{
 				var minusResult = m_score1 - m_score2;
-				if (minusResult == 1) score = "Advantage player1";
-				else if (minusResult == -1) score = "Advantage player2";
-				else if (minusResult >= 2) score = "Win for player1";
-				else score = "Win for player2";
+				if (minusResult == 1)
+				{
+					score = "Advantage player1";
+				}
+				else if (minusResult == -1)
+				{
+					score = "Advantage player2";
+				}
+				else if (minusResult >= 2)
+				{
+					score = "Win for player1";
+				}
+				else
+				{
+					score = "Win for player2";
+				}
 			}
-			else {
-				for (var i = 1; i < 3; i++) {
-					if (i == 1) tempScore = m_score1;
-					else { score += "-"; tempScore = m_score2; }
-					switch (tempScore) {
-						case 0:
-							score += "Love";
-							break;
-						case 1:
-							score += "Fifteen";
-							break;
-						case 2:
-							score += "Thirty";
-							break;
-						case 3:
-							score += "Forty";
-							break;
+			else
+			{
+				for (var i = 1; i < 3; i++)
+				{
+					if (i == 1)
+					{
+						tempScore = m_score1;
 					}
+					else
+					{
+						score += "-"; tempScore = m_score2;
+					}
+					score += ConvertScoreToString(tempScore);
 				}
 			}
 			return score;
+		}
+
+		private static string ConvertScoreToString(int score)
+		{
+			switch (score)
+			{
+				case 0:
+					return "Love";
+				case 1:
+					return "Fifteen";
+				case 2:
+					return "Thirty";
+				case 3:
+					return "Forty";
+			}
+			throw new ArgumentOutOfRangeException(nameof(score));
 		}
 	}
 }
