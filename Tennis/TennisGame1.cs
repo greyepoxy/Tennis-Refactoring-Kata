31d95495a299a9df18c333a561cf85dc37104caf
Justin <greyepoxy2@gmail.com>
2017-03-14 13:33:34 -0700

    GetScore now short circuit returns

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -25,44 +25,39 @@ namespace Tennis
 
 		public string GetScore()
 		{
-			string score = "";
-			var tempScore = 0;
 			if (m_score1 == m_score2)
 			{
 				if (m_score1 < 3)
 				{
-					score = ConvertScoreToString(m_score1) + "-" + "All";
+					return ConvertScoreToString(m_score1) + "-" + "All";
 				}
-				else
-				{
-					score = "Deuce";
-				}
+
+				return "Deuce";
 			}
 			else if (m_score1 >= 4 || m_score2 >= 4)
 			{
 				var minusResult = m_score1 - m_score2;
 				if (minusResult == 1)
 				{
-					score = "Advantage player1";
+					return "Advantage player1";
 				}
 				else if (minusResult == -1)
 				{
-					score = "Advantage player2";
+					return "Advantage player2";
 				}
 				else if (minusResult >= 2)
 				{
-					score = "Win for player1";
+					return "Win for player1";
 				}
 				else
 				{
-					score = "Win for player2";
+					return "Win for player2";
 				}
 			}
 			else
 			{
-				score = ConvertScoreToString(m_score1) + "-" + ConvertScoreToString(m_score2);
+				return ConvertScoreToString(m_score1) + "-" + ConvertScoreToString(m_score2);
 			}
-			return score;
 		}
 
 		private static string ConvertScoreToString(int score)
